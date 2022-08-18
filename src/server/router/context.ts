89fa674d2b0a0dce73f2ec62841ec10c9810449f
Eric Bai <baixu.must@gmail.com>
2022-08-18 22:51:53 +0800

    get rid of unneccessary settings

--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -1,15 +1,13 @@
 // src/server/router/context.ts
 import * as trpc from "@trpc/server";
-import * as trpcNext from "@trpc/server/adapters/next";
 import { prisma } from "../db/client";
 
-type CreateContextOptions = {};
 
 /** Use this helper for:
  * - testing, where we dont have to Mock Next.js' req/res
  * - trpc's `createSSGHelpers` where we don't have req/res
  **/
-export const createContextInner = async (opts: CreateContextOptions) => {
+export const createContextInner = async () => {
   return {
     prisma,
   };
@@ -19,10 +17,8 @@ export const createContextInner = async (opts: CreateContextOptions) => {
  * This is the actual context you'll use in your router
  * @link https://trpc.io/docs/context
  **/
-export const createContext = async (
-  opts: trpcNext.CreateNextContextOptions,
-) => {
-  return await createContextInner({});
+export const createContext = async () => {
+  return await createContextInner();
 };
 
 type Context = trpc.inferAsyncReturnType<typeof createContext>;
