06786efa6535812ca9e50647ed9b97bf95849490
Eric Bai <baixu.must@gmail.com>
2022-08-30 11:21:16 +0800

    basic functionality is done

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -4,7 +4,10 @@ import { appRouter } from '@server/router';
 import { getSlugs, PostMeta } from '@server/router/utils';
 import { serialize } from 'next-mdx-remote/serialize'
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
-
+import rehypeHighlight from 'rehype-highlight'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from 'rehype-slug'
+import "highlight.js/styles/atom-one-dark.css";
 type Params = {
   params: {
     slug: string
@@ -24,7 +27,15 @@ export const getStaticProps = async ({ params }: Params) => {
    */
   const { post } = await ssg.fetchQuery('blog.post', params.slug);
   const { content, meta } = post;
-  const mdxSource = await serialize(content);
+  const mdxSource = await serialize(
+    content,
+    {
+      mdxOptions: {
+        rehypePlugins: [rehypeSlug, rehypeHighlight, [rehypeAutolinkHeadings, { behavior: 'wrap' }]],
+      },
+    }
+  );
+
   return {
     props: {
       post: { meta, source: mdxSource },
@@ -56,7 +67,7 @@ const postDetail = ({ post }: { post: MDXpost }) => {
 
   return (
     <div className="min-h-screen flex flex-col">
-      <div className='prose mx-auto relative w-full px-6 py-12 bg-white shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 md:max-w-3xl md:mx-auto lg:max-w-4xl lg:pt-16 lg:pb-28'>
+      <div className='prose  relative w-full px-12 py-12 bg-white shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 md:max-w-3xl md:mx-auto lg:max-w-4xl lg:pt-20 lg:pb-28'>
         <h1>{post.meta.title}</h1>
         <MDXRemote {...post.source} />
       </div>
